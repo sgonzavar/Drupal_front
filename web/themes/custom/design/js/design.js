ee8e3dea6e381ca5375cc624e82a40e887df52d7
Tiago <sgonzalezvargas.7cc@gmail.com>
2024-09-02 07:25:10 -0500

    Feat: create new module

---
+++ web/themes/custom/design/js/design.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * design behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.design = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
